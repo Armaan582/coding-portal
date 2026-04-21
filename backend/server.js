f67a16fd30fbc8a8b8a015a3014da36d28dd7c08
Armaan <armaanjots215@gamil.com>
2026-04-21 21:13:56 +0530

    fix cors final

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://coding-portal-navy.vercel.app' ],
   credentials: true
 }));
 app.use(express.json());
